60175f4e3176af6eef25d2da535d19045b988a6e
hjy <hjy666666@sjtu.edu.cn>
2025-08-26 15:15:35 +0800

    simple main finish

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+)
 
 var (
 	card = []string{"", "", "", ""}
@@ -29,6 +32,30 @@ func ParseCard() {
 	}
 }
 
-func main() {
+func dfs() {
+	//TODO
+}
 
+func calculate() {
+	//TODO
+}
+
+func tryParentheses(a float64, b float64, c float64, d float64) string {
+	//TODO
+	return ""
+}
+
+func main() {
+	for i := 0; i < 4; i++ {
+		_, err := fmt.Scan(&card[i])
+		if err != nil {
+			log.Fatalln("Failed to read input:", err)
+		}
+	}
+	log.Println("Input cards:", card)
+	ParseCard()
+	log.Println("Parsed numbers:", num)
+	dfs()
+	log.Println("No Answer")
+	return
 }
